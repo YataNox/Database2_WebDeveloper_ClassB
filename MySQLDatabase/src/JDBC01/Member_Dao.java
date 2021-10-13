bcf889d1a4ccd2dd0e18ae913219cb63dd33ef8e
kim <rladnwls0523@naver.com>
2021-10-13 21:12:38 +0900

    Update Memberlist Manager System - MySQL.version

--- MySQLDatabase/src/JDBC01/Member_Dao.java
+++ MySQLDatabase/src/JDBC01/Member_Dao.java
@@ -1,5 +1,16 @@
 package JDBC01;
 
-public class Member_Dao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class Member_Dao {
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	private Member_Dao() {}
+	private Member_Dao itc = new Member_Dao();
+	public Member_Dao getInstance() {return itc;}
 }
+
